c9296878cca0a91833d890e4552cf412de718e8b
vee.king27@gmail.com <30303455+vinceyangeles@users.noreply.github.com>
2017-07-27 09:42:24 +1000

    Neon Nebula Prototype
    Consists of basic Player Movement Prototype, Player Respawn and Camera Follow

---
+++ 2D Game/Assets/SCRIPTS/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public bool grounded = false;
+    public Transform groundedEnd;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+        Raycasting();
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 8f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * 8f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500f);
+        }
+    }
+
+    void Raycasting()
+    {
+        grounded = Physics2D.Linecast(this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
+}
