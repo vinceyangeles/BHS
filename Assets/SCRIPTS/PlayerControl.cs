cc9dd78f2fa49d8290acd486bfad94385ddec63e
vee.king27@gmail.com <30303455+vinceyangeles@users.noreply.github.com>
2017-08-03 10:41:39 +1000

    Character Animation imported in Game
    In this commit, i first removed unwanted files in the project and just left the Assets folder. I also added Christy's animation and have it replace the plain cube.

---
+++ Assets/SCRIPTS/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public bool grounded = false;
+    public Transform groundedEnd;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+        Raycasting();
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 8f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * 8f * Time.deltaTime);
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500f);
+        }
+    }
+
+    void Raycasting()
+    {
+        grounded = Physics2D.Linecast(this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
+}
